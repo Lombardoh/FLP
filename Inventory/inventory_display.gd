0ba648f098c882c93f79f514c22fe39c6d6c4452
lombardoh <lombardo.h@hotmail.com>
2023-10-27 18:53:14 -0300

    fix: last name fixing for now

--- Inventory/inventory_display.gd
+++ Inventory/inventory_display.gd
@@ -1,9 +1,13 @@
 extends GridContainer
 
-var inventory = preload("res://Inventory/inventory.tres")
+var inventory = preload("res://Items/global_item_list.tres")
+var inventory_slot_display = preload("res://inventory/inventory_slot_display.tscn")
 
 func _ready():
 	inventory.connect("items_changed", _on_items_changed)
+	for item in inventory.items.size():
+		var instance = inventory_slot_display.instantiate()
+		add_child(instance)
 	update_inventory_display()
 
 func update_inventory_display() -> void:
@@ -14,7 +18,7 @@ func update_inventory_slot_display(item_index: int) -> void:
 	var inventory_slot_display = get_child(item_index)
 	var item: Item = inventory.items[item_index]
 	inventory_slot_display.display_item(item)
-	
+
 func _on_items_changed(indeses: Array[int]) -> void:
 	for item_index in indeses:
 		update_inventory_slot_display(item_index)
