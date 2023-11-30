dffa58a24f9af83bfdebc1cddec183feff02d0f8
lombardoh <lombardo.h@hotmail.com>
2023-11-30 15:11:28 -0300

    feat: finished tilemap navigation

--- NPC/enemies/test_enemy/test_enemy.gd
+++ NPC/enemies/test_enemy/test_enemy.gd
@@ -1,15 +1,17 @@
 extends NPC
 
-var active_follow: bool = true
-
 func _ready() -> void:
 	progress_bar.max_value = health
 	progress_bar.value = health
-	add_target(get_tree().get_first_node_in_group("target"))
+#	add_target(get_tree().get_first_node_in_group("target"))
+	
 
 func _physics_process(_delta) -> void:
 	move_and_slide()
 
-func _on_detection_range_body_entered(body) -> void:
-	add_target(body)
+
+func _on_detection_range_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		add_target(body)
+
 
--- state_machine/states/idle_state.gd
+++ state_machine/states/idle_state.gd
@@ -1,6 +1,8 @@
 extends State
 class_name IdleState
 
+var frame_check: int = 0
+
 func enter_state() -> void:
 	actor.velocity = Vector2()
 	animation_player.play("idle")
@@ -8,5 +10,15 @@ func enter_state() -> void:
 func physics_update(_delta: float):
 	if actor.targets.size() > 0:
 		actor.set_new_target()
-		Transition.emit(self, "FollowState")
-		
+
+		if ray_cast and actor.current_target:
+			var global_mouse_position = get_viewport().get_mouse_position()
+
+			ray_cast.target_position = actor.current_target.position - actor.position
+
+			if frame_check > 0 and not ray_cast.is_colliding():
+				frame_check = 0
+				Transition.emit(self, "FollowState")
+			else:
+				frame_check += 1
+
